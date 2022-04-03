4df42b40135f2013f5be312e3399921342e372f4
Žygimantas Kadiša <kadisa917@gmail.com>
2022-04-04 00:35:39 +0300

    Consoles WIP

--- Assets/Scripts/Console/ConsoleModule.cs
+++ Assets/Scripts/Console/ConsoleModule.cs
@@ -6,9 +6,27 @@ using UnityEngine;
 [RequireComponent(typeof(MeshRenderer))]
 public class ConsoleModule : MonoBehaviour
 {
-    [SerializeField] private Material ConnectedMaterial;
-    [SerializeField] private Material DisconnectedMaterial;
+    [Serializable]
+    public struct MaterialData
+    {
+        public Material Material;
+        public int MaterialIndexInMesh;
+        public Color LightColor;
+        public float LightIntensity;
+        public float LightRange;
+    }
 
+    [Serializable]
+    public struct MaterialAnimationData
+    {
+        public MaterialData ConnectedMaterial;
+        public MaterialData DisconnectedMaterial;
+    }
+
+    [SerializeField] private MaterialAnimationData MaterialAnimations;
+
+    [SerializeField] private Light Light;
+    [SerializeField] private Light LightSpot;
     [SerializeField] private Transform _CableAttachementPosition;
     public Transform CableAttachementPosition => _CableAttachementPosition;
 
@@ -24,12 +42,13 @@ public class ConsoleModule : MonoBehaviour
     private void Awake()
     {
         MeshRenderer = GetComponent<MeshRenderer>();
-        MeshRenderer.material = DisconnectedMaterial;
+        AssignMaterialData(MaterialAnimations.ConnectedMaterial);
+        AssignMaterialData(MaterialAnimations.DisconnectedMaterial);
     }
 
     public void Disconnected()
     {
-        MeshRenderer.material = DisconnectedMaterial;
+        AssignMaterialData(MaterialAnimations.DisconnectedMaterial);
         ConnectedCablePlug.CancelSnapping();
         OnCableDisconnected?.Invoke();
         IsBeingConnectedTo = false;
@@ -37,7 +56,7 @@ public class ConsoleModule : MonoBehaviour
 
     public void InitiallyConnected()
     {
-        MeshRenderer.material = ConnectedMaterial;
+        AssignMaterialData(MaterialAnimations.ConnectedMaterial);
         ConnectedCablePlug.SnapToOnEndMouseDrag(this);
         OnCableInitiallyConnected?.Invoke();
         IsBeingConnectedTo = true;
@@ -72,10 +91,21 @@ public class ConsoleModule : MonoBehaviour
         if (ConnectedCablePlug == null)
             return;
 
-        if (other.gameObject == ConnectedCablePlug.gameObject) 
+        if (other.gameObject == ConnectedCablePlug.gameObject)
         {
             Disconnected();
             ConnectedCablePlug = null;
         }
+    }
+
+    private void AssignMaterialData(MaterialData data)
+    {
+        var mats = MeshRenderer.materials;
+        mats[data.MaterialIndexInMesh] = data.Material;
+        MeshRenderer.materials = mats;
+        Light.color = data.LightColor;
+        Light.intensity = data.LightIntensity;
+        Light.range = data.LightRange;
+        LightSpot.color = data.LightColor;
     }
 }
