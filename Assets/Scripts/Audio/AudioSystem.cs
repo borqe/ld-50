a635fcda6259ee3c6f66784e7a0448f73dfe4d8c
Robertas Paulauskas <paulauskx@gmail.com>
2022-04-04 22:25:12 +0300

    Micro audio system

---
+++ Assets/Scripts/Audio/AudioSystem.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Audio
+{
+    public class AudioSystem: Singleton<AudioSystem>
+    {
+        [SerializeField] private AudioSource _aiSource;
+        [SerializeField] private AudioSource _musicSource;
+        [SerializeField] private AudioFiles _files;
+
+        private void OnEnable()
+        {
+            _musicSource.clip = _files.Music;
+            _musicSource.loop = true;
+            _musicSource.playOnAwake = true;
+        }
+
+        public void PlayVoiceLine(string line)
+        {
+            _aiSource.Stop();
+            if (_files.VoiceLines.ContainsKey(line))
+            {
+                _aiSource.clip = _files.VoiceLines[line];
+            }
+            _aiSource.Play();
+        }
+    }
+}
